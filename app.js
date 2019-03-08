1d21b18ec8b1efc7c975688557195b666200ffa8
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-03-08 11:54:16 +0200

    First code

---
+++ app.js
@@ -0,0 +1,52 @@
+// budgetController
+const budgetController = (function functionName() {})();
+
+// UIController
+const UIController = (function() {
+  const domStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputButton: ".add__btn"
+  };
+  return {
+    getInput: () => {
+      return {
+        type: document.querySelector(domStrings.inputType).value,
+        description: document.querySelector(domStrings.inputDescription).value,
+        value: document.querySelector(domStrings.inputValue).value
+      };
+    },
+
+    getDomStrings: () => domStrings
+  };
+})();
+
+// controller
+const controller = (function(budgetCtrl, uiCtrl) {
+  const dom = UIController.getDomStrings();
+
+  const ctrlAddItem = () => {
+    //Get the field input data
+    const input = UIController.getInput();
+    console.log(input);
+
+    //Add the item to the budget controller
+
+    //Add a new item to the UI
+
+    //Calculcate the budget
+
+    //Display the Budget
+  };
+
+  document
+    .querySelector(dom.inputButton)
+    .addEventListener("click", ctrlAddItem);
+
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(budgetController, UIController);
